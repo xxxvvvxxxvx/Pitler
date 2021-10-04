5837f07e2e5501e50d2ba21773dfa568c0c2403f
dead inside <90174775+xxxvvvxxxvx@users.noreply.github.com>
2021-10-04 06:43:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+'''
+Ēriks M
+27.09
+'''
+
+t = ('asas', 'hgh', 'qe4', 'jhjk')
+print(t)
+print(t[2])
+print(t[-4])
+print(t[1:3])
+print(t[:3])
+print(t[1:])
+print(t[-3:-1])
+print(t[-2:])
+
+x = ['orange', 'red', 'blue', 'yellow', 'green', 'red', 'yellow']
+print(x)
+x.count('red')
+x.index('orange')
+
+s=set()
+print(s)
+s.add(1)
+print(s)
+s.add(2)
+print(s)
+s.add(1)
+print(s)
+
+
+list1= ["gag", "aadad", "ad"]
+set(list1)
